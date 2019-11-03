fa2ece3a9deb7ef9bdc90a2218ef0465e76866f8
marsovc1 <koki.549@gmail.com>
2019-11-03 11:36:44 +0100

    Ugotovil osnovno idejo podatkov

--- Poberi_podatke.py
+++ Poberi_podatke.py
@@ -6,7 +6,7 @@ import requests
 import orodja
 import pandas
 
-#shranimo HTML dadoteke pod imeni transfer20xy (glede na leto), v mapo html s pomočjo datoteke 'orodja'
+#shranimo HTML datoteke pod imeni transfer20xy (glede na leto), v mapo html s pomočjo datoteke 'orodja'
 
 link = r'C:\Users\marsovc\Desktop\Fmf\Financna tretji letnik\Programiranje 1\Projektna-naloga-PROG1\html\transfer'
 
@@ -17,17 +17,13 @@ for i in range(2000,2020):
     else:
         shrani_spletno_stran('https://en.wikipedia.org/wiki/'+ str(i) + '-' + str(i-1999) +'_FC_Barcelona_season', link + str(i), vsili_prenos=False)
  """
-
-# <h3><span class="mw-headline" id="Transfers">Transfers</span><span class - oblika teksta pri transferju
-
+#s pomočjo orodja poberem vsebino
 tekst = orodja.vsebina_datoteke(link+'2000')
+#dodam v BSoup
 soup = BeautifulSoup(tekst, 'lxml')
 
+#tabela transfer out
 tekst = soup.find('table',class_='wikitable sortable', style="text-align: center;")
-ahref = tekst.find_all('a')
-transfer_in = []
-for link in ahref:
-    neki.append(link.get('title'))
-neki = neki[3:]
-print(neki)
 
+#Cilj: poberem Title oziroma td, če title ne obstaja
+tekst = tekst.find_all('tr')
